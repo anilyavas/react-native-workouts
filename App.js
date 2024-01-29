d8bebf215f1787a1aa0986f86314959bb616a4c4
Anıl <anil@Anl-MacBook-Air.local>
2024-01-29 14:01:11 +0300

    rendering list of exercises

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import exercises from './assets/data/exercises.json';
 
+function ExerciseListItem({ item }) {
+  return (
+    <View style={styles.exerciseContainer}>
+      <Text style={styles.exerciseName}>{item.name}</Text>
+      <Text style={styles.exerciseSubtitle}>
+        {item.muscle.toUpperCase()} | {item.equipment.toUpperCase()}
+      </Text>
+    </View>
+  );
+}
+
 export default function App() {
-  const exercise = exercises[0];
   return (
     <View style={styles.container}>
-      <View style={styles.exerciseContainer}>
-        <Text style={styles.exerciseName}>{exercise.name}</Text>
-        <Text style={styles.exerciseSubtitle}>
-          {exercise.muscle.toUpperCase()} | {exercise.equipment.toUpperCase()}
-        </Text>
-      </View>
+      <FlatList
+        data={exercises}
+        renderItem={({ item }) => <ExerciseListItem item={item} />}
+      />
+
       <StatusBar style='auto' />
     </View>
   );
