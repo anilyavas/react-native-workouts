1db5de0e2b7992613020c9145979ae44f4385b72
Anıl <anil@Anl-MacBook-Air.local>
2024-01-31 12:49:33 +0300

    stack navigator

--- src/app/[name].js
+++ src/app/[name].js
@@ -1,6 +1,7 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ScrollView } from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import exercises from '../../assets/data/exercises.json';
+import { Stack } from 'expo-router';
 
 export default function ExerciseDetailsScreen() {
   const params = useLocalSearchParams();
@@ -10,18 +11,30 @@ export default function ExerciseDetailsScreen() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.exerciseName}>{exercise.name}</Text>
-      <Text style={styles.exerciseSubtitle}>
-        <Text style={styles.subValue}>{exercise.muscle}</Text> | {''}
-        <Text style={styles.subValue}>{exercise.equipment}</Text>
-      </Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <Stack.Screen options={{ title: exercise.name }} />
+      <View style={styles.panel}>
+        <Text style={styles.exerciseName}>{exercise.name}</Text>
+        <Text style={styles.exerciseSubtitle}>
+          <Text style={styles.subValue}>{exercise.muscle}</Text> | {''}
+          <Text style={styles.subValue}>{exercise.equipment}</Text>
+        </Text>
+      </View>
+      <View style={styles.panel}>
+        <Text style={styles.instructions}>{exercise.instructions}</Text>
+      </View>
+    </ScrollView>
   );
 }
 const styles = StyleSheet.create({
   container: {
     padding: 10,
+    gap: 10,
+  },
+  panel: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
   },
   exerciseName: {
     fontSize: 20,
@@ -32,5 +45,9 @@ const styles = StyleSheet.create({
   },
   subValue: {
     textTransform: 'capitalize',
+  },
+  instructions: {
+    fontSize: 16,
+    lineHeight: 22,
   },
 });
