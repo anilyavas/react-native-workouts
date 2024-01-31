018d6a8ec983e38bd1f3dd13af228a3da1ca2fb6
Anıl <anil@Anl-MacBook-Air.local>
2024-01-31 15:19:36 +0300

    fetch data

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, FlatList } from 'react-native';
+import { StyleSheet, View, FlatList, ActivityIndicator } from 'react-native';
 import exercises from '../../assets/data/exercises.json';
 import { ExerciseListItem } from '../../src/components/ExerciseListItem';
+import { useQuery } from '@tanstack/react-query';
+import { gql, request } from 'graphql-request';
 
-export default function App() {
+const url =
+  'https://campigliamarittima.stepzen.net/api/reeling-stingray/__graphql';
+
+const exercisesQuery = gql`
+  query exercises($muscle: String, $name: String) {
+    exercises(muscle: $muscle, name: $name) {
+      name
+      muscle
+    }
+  }
+`;
+
+export default function ExercisesScreen() {
+  const { data, isLoading } = useQuery({
+    queryKey: ['exercises'],
+    queryFn: async () => {
+      return request(url, exercisesQuery);
+    },
+  });
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    return <Text>Failed to fetch</Text>;
+  }
   return (
     <View style={styles.container}>
       <FlatList
         contentContainerStyle={{ gap: 5 }}
-        data={exercises}
+        data={data}
         keyExtractor={(item, index) => item.name + index}
         renderItem={({ item }) => <ExerciseListItem item={item} />}
       />
