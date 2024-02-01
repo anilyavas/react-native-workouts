532d7fe8d682ec1d4b25060fa17d52c7d3b10811
Anıl <anil@Anl-MacBook-Air.local>
2024-02-01 14:17:08 +0300

    exercise details screen data fetching from gql

--- src/app/[name].js
+++ src/app/[name].js
@@ -1,14 +1,45 @@
-import { View, Text, StyleSheet, ScrollView } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  ActivityIndicator,
+} from 'react-native';
 import { useLocalSearchParams } from 'expo-router';
 import exercises from '../../assets/data/exercises.json';
 import { Stack } from 'expo-router';
 import { useState } from 'react';
+import { gql } from 'graphql-request';
+import { useQuery } from '@tanstack/react-query';
+import { graphqlClient } from '../graphqlClient';
 
+const exerciseQuery = gql`
+  query exercises($name: String) {
+    exercises(name: $name) {
+      name
+      muscle
+      instructions
+      equipment
+    }
+  }
+`;
 export default function ExerciseDetailsScreen() {
+  const { name } = useLocalSearchParams();
+  const { data, isLoading, error } = useQuery({
+    queryKey: ['exercises', name],
+    queryFn: () => graphqlClient.request(exerciseQuery, { name }),
+  });
+
   const [isInstructionExpanded, setIsInstructionExpanded] = useState(false);
 
-  const params = useLocalSearchParams();
-  const exercise = exercises.find((item) => item.name === params.name);
+  if (isLoading) {
+    return <ActivityIndicator />;
+  }
+  if (error) {
+    return <Text>Failed to fetch data</Text>;
+  }
+  const exercise = data.exercises[0];
+
   if (!exercise) {
     return <Text>Exercise Not Found</Text>;
   }
--- src/app/index.js
+++ src/app/index.js
@@ -1,18 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, FlatList, ActivityIndicator } from 'react-native';
-import exercises from '../../assets/data/exercises.json';
+import {
+  StyleSheet,
+  View,
+  Text,
+  FlatList,
+  ActivityIndicator,
+} from 'react-native';
 import { ExerciseListItem } from '../../src/components/ExerciseListItem';
 import { useQuery } from '@tanstack/react-query';
-import { gql, request } from 'graphql-request';
-
-const url =
-  'https://campigliamarittima.stepzen.net/api/reeling-stingray/__graphql';
+import { gql } from 'graphql-request';
+import client from '../graphqlClient';
 
 const exercisesQuery = gql`
   query exercises($muscle: String, $name: String) {
     exercises(muscle: $muscle, name: $name) {
       name
       muscle
+      equipment
     }
   }
 `;
@@ -20,21 +24,13 @@ const exercisesQuery = gql`
 export default function ExercisesScreen() {
   const { data, isLoading, error } = useQuery({
     queryKey: ['exercises'],
-    queryFn: async () => {
-      return request({
-        url,
-        document: exercisesQuery,
-        requestHeaders: {
-          Authorization:
-            'apikey campigliamarittima::stepzen.io+1000::e8f1f13d8a937cc4f7e68c32a1cbef64070a0143ee2bb5d22b09933c06eab84d',
-        },
-      });
-    },
+    queryFn: () => client.request(exercisesQuery),
   });
   if (isLoading) {
     return <ActivityIndicator />;
   }
   if (error) {
+    console.log(error);
     return <Text>Failed to fetch</Text>;
   }
   return (
